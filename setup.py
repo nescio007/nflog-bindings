9ce2e30e0969fee04ccfec178a9257c2f2acc182
Johannes Krupp <johannes.krupp@cispa.saarland>
2019-07-08 14:26:30 +0200

    single source version number

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
+import os
+import re
 from setuptools import setup, Extension
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^#define NFLOG_BINDINGS_VERSION ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 nflog = Extension('_nflog', sources=['nflog.c', 'nflog_common.c', 'nflog_utils.c', 'exception.c', 'libnetfilter_log.i'], include_dirs=['.'], libraries=['netfilter_log'])
 
 setup(
     name='nflog-bindings',
-    version='0.0.1',
+    version=find_version("nflog_version.h"),
     ext_modules=[nflog],
     py_modules=['nflog'],
     url='',
